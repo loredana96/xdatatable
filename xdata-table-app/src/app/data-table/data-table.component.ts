9599aeabb73beffa48f40e025bcd8c902f5557f3
loredana96 <loredanatodoran15@gmail.com>
2023-08-19 15:00:07 +0300

    Initial setup of the table #4

---
+++ xdata-table-app/src/app/data-table/data-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent {
+
+}
---
+++ xdata-table-app/src/app/utils/clients.ts
@@ -0,0 +1,9 @@
+export interface IClient {
+    name: string,
+    type: string,
+    email: string,
+    phoneNo: string,
+    companyName: string,
+    address: string,
+    children: IClient[]
+}
